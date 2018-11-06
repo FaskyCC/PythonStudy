5becc9546482d9518f1b364667219dcb3ff648a5
FaskyCC <1791853817@qq.com>
2018-11-06 21:08:03 +0800

    模块练习

---
+++ Study/模块/M01.py
@@ -0,0 +1,12 @@
+#创建一个模块,包含一个类,一个函数,一个输出语句
+class Student():
+    def __init__(self,name = "No",age = 0):
+        self.name = name
+        self.age = age
+    def selfIntroduce(self):
+        print("你好,我的名字是{0},我今年{1}岁了".format(self.name,self.age))
+
+def sayHello():
+    print("Hello World")
+
+print("这是一个模块")
---
+++ Study/模块/testMoudle1.py
@@ -0,0 +1,5 @@
+import M01
+
+stu = M01.Student("菠萝", 18)
+stu.selfIntroduce()
+M01.sayHello()
