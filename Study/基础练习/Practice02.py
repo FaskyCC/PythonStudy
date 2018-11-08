0ca5743fd3128433b44a4ef1ff74c81ff3a2a6c4
FaskyCC <1791853817@qq.com>
2018-11-08 20:45:15 +0800

    基础练习01&02

---
+++ Study/基础练习/Practice02.py
@@ -0,0 +1,13 @@
+#打印菱形
+
+def draw(num):
+    a="*"*(2*(c-num)+1)
+    print(a.center(b,' '))
+    if num!=1:
+        draw(num-1)
+        print(a.center(b,' '))
+for num in range(1,5):
+    num = int(num)
+    c = num
+    b = 11
+    draw(num)
